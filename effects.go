db73314d659a540115441de5aeb64b9a21e0f165
vladislav <ve@incsub.com>
2022-12-03 17:15:41 +0100

    Refactor effects

---
+++ effects.go
@@ -0,0 +1,105 @@
+package main
+
+type pixelateMode byte
+
+const (
+	ModePixelate     pixelateMode = iota
+	ModeAndNormalize pixelateMode = iota
+)
+
+func PixelateImage(bfr *ImageBuffer, mode pixelateMode) *ImageBuffer {
+	square := squareSize
+	edit := make([]*Pixel, len(bfr.pixels))
+
+	for y := 0; y < bfr.height; y += square {
+		for x := 0; x < bfr.width; x += square {
+			var normalized Pixel
+
+			if ModeAndNormalize == mode {
+				p := make([]*Pixel, 0, square*square)
+
+				for i := 0; i < square; i++ {
+					for j := 0; j < square; j++ {
+						dy := y + i
+						if dy >= bfr.height {
+							continue
+						}
+						dx := x + j
+						if dx >= bfr.width {
+							continue
+						}
+						px := bfr.pixels[dy*bfr.width+dx]
+						p = append(p, px)
+					}
+				}
+				normalized = normalizeColors_RGBA(p, 4)[0]
+			} else {
+				normalized = bfr.pixels[y*bfr.width+x].Clone()
+			}
+
+			for i := 0; i < square; i++ {
+				for j := 0; j < square; j++ {
+					dy := y + i
+					if dy >= bfr.height {
+						continue
+					}
+					dx := x + j
+					if dx >= bfr.width {
+						continue
+					}
+					edit[dy*bfr.width+dx] = &normalized
+				}
+			}
+		}
+	}
+	return &ImageBuffer{
+		width:  bfr.width,
+		height: bfr.height,
+		pixels: edit}
+}
+
+func ConstrainImage(bfr *ImageBuffer, palette Palette) *ImageBuffer {
+	square := squareSize
+	edit := make([]*Pixel, len(bfr.pixels))
+
+	for y := 0; y < bfr.height; y += square {
+		for x := 0; x < bfr.width; x += square {
+			p := make([]*Pixel, 0, square*square)
+
+			for i := 0; i < square; i++ {
+				for j := 0; j < square; j++ {
+					dy := y + i
+					if dy >= bfr.height {
+						continue
+					}
+					dx := x + j
+					if dx >= bfr.width {
+						continue
+					}
+					px := bfr.pixels[dy*bfr.width+dx]
+					p = append(p, px)
+				}
+			}
+
+			normalized := normalizeColors_RGBA(p, 4)[0]
+			closest := palette.ClosestTo(normalized)
+			for i := 0; i < square; i++ {
+				for j := 0; j < square; j++ {
+					dy := y + i
+					if dy >= bfr.height {
+						continue
+					}
+					dx := x + j
+					if dx >= bfr.width {
+						continue
+					}
+					edit[dy*bfr.width+dx] = &closest
+				}
+			}
+		}
+	}
+	return &ImageBuffer{
+		width:  bfr.width,
+		height: bfr.height,
+		pixels: edit}
+}
--- main.go
+++ main.go
@@ -46,49 +46,8 @@ func printAveragedImage(palette Palette) {
 		outputFname = "average-image-palette.jpg"
 		paletteFname = "image-palette.jpg"
 	}
-	square := squareSize
-	edit := make([]*Pixel, len(bfr.pixels))
 
-	for y := 0; y < bfr.height; y += square {
-		for x := 0; x < bfr.width; x += square {
-			p := make([]*Pixel, 0, square*square)
-
-			for i := 0; i < square; i++ {
-				for j := 0; j < square; j++ {
-					dy := y + i
-					if dy >= bfr.height {
-						continue
-					}
-					dx := x + j
-					if dx >= bfr.width {
-						continue
-					}
-					px := bfr.pixels[dy*bfr.width+dx]
-					p = append(p, px)
-				}
-			}
-
-			normalized := normalizeColors_RGBA(p, 4)[0]
-			closest := palette.ClosestTo(normalized)
-			for i := 0; i < square; i++ {
-				for j := 0; j < square; j++ {
-					dy := y + i
-					if dy >= bfr.height {
-						continue
-					}
-					dx := x + j
-					if dx >= bfr.width {
-						continue
-					}
-					edit[dy*bfr.width+dx] = &closest
-				}
-			}
-		}
-	}
-	b2 := &ImageBuffer{
-		width:  bfr.width,
-		height: bfr.height,
-		pixels: edit}
+	b2 := ConstrainImage(bfr, palette)
 	b2.ToJPEGFile(outputFname)
 	printPaletteImage(palette, paletteFname)
 }
@@ -96,78 +55,13 @@ func printAveragedImage(palette Palette) {
 func printHarsherPixelatedImage() {
 	file := filepath.Join("testdata", "sample.jpg")
 	bfr := FromJPEG(file)
-	square := squareSize
-	edit := make([]*Pixel, len(bfr.pixels))
-
-	for y := 0; y < bfr.height; y += square {
-		for x := 0; x < bfr.width; x += square {
-			p := make([]*Pixel, 0, square*square)
-
-			for i := 0; i < square; i++ {
-				for j := 0; j < square; j++ {
-					dy := y + i
-					if dy >= bfr.height {
-						continue
-					}
-					dx := x + j
-					if dx >= bfr.width {
-						continue
-					}
-					px := bfr.pixels[dy*bfr.width+dx]
-					p = append(p, px)
-				}
-			}
-
-			normalized := normalizeColors_RGBA(p, 4)[0]
-			for i := 0; i < square; i++ {
-				for j := 0; j < square; j++ {
-					dy := y + i
-					if dy >= bfr.height {
-						continue
-					}
-					dx := x + j
-					if dx >= bfr.width {
-						continue
-					}
-					edit[dy*bfr.width+dx] = &normalized
-				}
-			}
-		}
-	}
-	b2 := &ImageBuffer{
-		width:  bfr.width,
-		height: bfr.height,
-		pixels: edit}
+	b2 := PixelateImage(bfr, ModeAndNormalize)
 	b2.ToJPEGFile("harsher.jpg")
 }
 
 func printPixelatedImage() {
 	file := filepath.Join("testdata", "sample.jpg")
 	bfr := FromJPEG(file)
-	square := squareSize
-	edit := make([]*Pixel, len(bfr.pixels))
-
-	for y := 0; y < bfr.height; y += square {
-		for x := 0; x < bfr.width; x += square {
-			normalized := bfr.pixels[y*bfr.width+x]
-			for i := 0; i < square; i++ {
-				for j := 0; j < square; j++ {
-					dy := y + i
-					if dy >= bfr.height {
-						continue
-					}
-					dx := x + j
-					if dx >= bfr.width {
-						continue
-					}
-					edit[dy*bfr.width+dx] = normalized
-				}
-			}
-		}
-	}
-	b2 := &ImageBuffer{
-		width:  bfr.width,
-		height: bfr.height,
-		pixels: edit}
+	b2 := PixelateImage(bfr, ModePixelate)
 	b2.ToJPEGFile("pixelated.jpg")
 }
