a34275d343c011b0cf094b912fa8c999d3ddf636
vladislav <ve@incsub.com>
2022-12-10 17:43:40 +0100

    Benchmarks

--- main_test.go
+++ main_test.go
@@ -19,3 +19,18 @@ func Benchmark_Pixelated_Write(b *testing.B) {
 		printPixelatedImage()
 	}
 }
+
+func Benchmark_Averaged(b *testing.B) {
+	file := filepath.Join("testdata", "sample.jpg")
+	bfr := pkg.FromJPEG(file)
+	palette := bfr.Palette(12) // from image itself
+	for i := 0; i < b.N; i++ {
+		pkg.ConstrainImage(bfr, palette)
+	}
+}
+
+func Benchmark_Averaged_Write(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		printAveragedImage(pkg.Palette{})
+	}
+}
