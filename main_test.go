a611f1b6f44ba9757c0393621437e5cf086d6c7a
vladislav <ve@incsub.com>
2022-12-10 09:25:25 +0100

    Add basic benchmarking

---
+++ main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"downsample/pkg"
+	"path/filepath"
+	"testing"
+)
+
+func Benchmark_Pixelated(b *testing.B) {
+	file := filepath.Join("testdata", "sample.jpg")
+	bfr := pkg.FromJPEG(file)
+	for i := 0; i < b.N; i++ {
+		pkg.PixelateImage(bfr, pkg.ModePixelate)
+	}
+}
