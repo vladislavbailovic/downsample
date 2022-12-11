9e653e0dc0fb9ce1fe1a1cb4ed1f9f6c3b7d81e6
vladislav <ve@incsub.com>
2022-12-11 04:57:05 +0100

    Reorganize package

--- pkg/color.go
+++ pkg/color.go
@@ -1,40 +1,9 @@
 package pkg
 
 import (
-	"fmt"
-	"image"
 	"image/color"
-	"image/jpeg"
-	"os"
 	"sort"
 )
-
-func FromJPEG(imgfile string) image.Image {
-	fp, err := os.Open(imgfile)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "unable to open image: %v", err)
-		return nil
-	}
-	defer fp.Close()
-
-	img, err := jpeg.Decode(fp)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "unable to decode JPEG: %v", err)
-	}
-
-	return img
-}
-
-func ImagePalette(src image.Image, size uint8) color.Palette {
-	bounds := src.Bounds()
-	all := make(color.Palette, 0, bounds.Max.X*bounds.Max.Y)
-	for y := 0; y < bounds.Max.Y; y++ {
-		for x := 0; x < bounds.Max.X; x++ {
-			all = append(all, src.At(x, y))
-		}
-	}
-	return normalizeColors_RGBA(all, size)
-}
 
 // TODO: improve this
 func normalizeColors_RGBA(pxl color.Palette, size uint8) color.Palette {
---
+++ pkg/image.go
@@ -0,0 +1,54 @@
+package pkg
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/jpeg"
+	"os"
+)
+
+func FromJPEG(imgfile string) image.Image {
+	fp, err := os.Open(imgfile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to open image: %v", err)
+		return nil
+	}
+	defer fp.Close()
+
+	img, err := jpeg.Decode(fp)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to decode JPEG: %v", err)
+	}
+
+	return img
+}
+
+func ImagePalette(src image.Image, size uint8) color.Palette {
+	bounds := src.Bounds()
+	all := make(color.Palette, 0, bounds.Max.X*bounds.Max.Y)
+	for y := 0; y < bounds.Max.Y; y++ {
+		for x := 0; x < bounds.Max.X; x++ {
+			all = append(all, src.At(x, y))
+		}
+	}
+	return normalizeColors_RGBA(all, size)
+}
+
+func ToPaletteImage(p color.Palette, tileSize int) image.Image {
+	img := image.NewRGBA(image.Rect(
+		0, 0, tileSize*len(p), tileSize))
+
+	for pos := 0; pos < len(p); pos++ {
+		c := p[pos]
+		for y := 0; y < tileSize; y++ {
+			for x := 0; x < tileSize; x++ {
+				img.Set(
+					(pos*tileSize)+x,
+					y,
+					c)
+			}
+		}
+	}
+	return img
+}
